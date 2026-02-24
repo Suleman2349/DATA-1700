1cc75e86f1af311d2228cc347640a94df2ff6b24
Suleman2349 <ahmedsuleman731@gmail.com>
2026-02-24 14:40:30 +0100

    assignment 1

---
+++ task_2.4/Javascript.js
@@ -0,0 +1,3 @@
+console.log("Hello from JavaScript!");
+console.log(5 + 10);
+alert ("hello world")
---
+++ task_3/Javascript.js
@@ -0,0 +1,10 @@
+const meterToYard=1.0936132983;
+
+function metersToYard(meters){
+    return meters * metersToYard;
+}
+const result = metersToYard(5);
+console.log(result);
+if(result>300){
+    console.log("Thats a really good drive");
+}
