dc82d7d2b090fd88488831b6faf7085560b38336
Suleman2349 <ahmedsuleman731@gmail.com>
2026-02-24 15:15:43 +0100

    changed task 3

--- task_3/Javascript.js
+++ task_3/Javascript.js
@@ -1,10 +1,9 @@
-const meterToYard=1.0936132983;
-
-function metersToYard(meters){
-    return meters * metersToYard;
+const meterToYards=1.0936132983;
+function oppgave3(meters){
+    let yards = meterToYards * meters;
+    console.log(yards + " yards");
+    if (yards > 300){
+        console.log("That is a really long golf drive")
+    }
 }
-const result = metersToYard(5);
-console.log(result);
-if(result>300){
-    console.log("Thats a really good drive");
-}
+oppgave3(400);
